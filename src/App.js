53a246c5b16cd5f41917e531dcbb228357355565
Akib <akibahmed2011@gmail.com>
2022-02-23 23:31:47 +0600

    cart item stored in localstorage and fetch from localstorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { sendCartData } from './store/cart-actions';
-import { fetchCartData } from './store/cart-actions';
 import React, {Suspense} from 'react';
 import Layout from './components/Layout/Layout';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import {Navigate} from 'react-router-dom';
-
+import { cartActions } from './store/cart-slice';
 const AuthPage = React.lazy(() => import('./pages/AuthPage'));
 const AdminDashboard = React.lazy(() => import('./pages/AdminDashboard'));
 
@@ -22,7 +20,18 @@ function App() {
   const isLoggedIn = useSelector(state => state.auth.token)
 
   useEffect(() => {
-    dispatch(fetchCartData());
+    const retrievedCart = localStorage.getItem('cart')
+        
+    if (retrievedCart) {
+      const cartObject = JSON.parse(retrievedCart)
+      dispatch(cartActions.replaceCart({
+      items: cartObject.items || [],
+      totalQuantity: cartObject.totalQuantity,
+      totalAmount: cartObject.totalAmount
+    }));
+    }
+
+    
   }, [dispatch]);
 
   useEffect(() => {
@@ -32,7 +41,13 @@ function App() {
     }
 
     if (cart.changed) {
-      dispatch(sendCartData(cart));
+
+      // Browser storage doesn't support object that's why I used JSON stringify
+      localStorage.setItem('cart', JSON.stringify({
+        items: cart.items,
+        totalQuantity: cart.totalQuantity,
+        totalAmount: cart.totalAmount
+      }))
     }
   }, [cart, dispatch]);
 
